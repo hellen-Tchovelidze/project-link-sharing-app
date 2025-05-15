4e2029a2f65d28d17424f0d0e858f662f642f49e
hellen-Tchovelidze <elenechovelidze@gmail.com>
2025-05-15 19:38:14 +0400

    feat(profile)

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,9 +1,11 @@
+import ProfileForm from '../../Components/__organisms/profilefrom'
 import React from 'react'
 
-const page = () => {
+
+export default function Home() {
   return (
-    <div>page</div>
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+    <ProfileForm />
+    </main>
   )
 }
-
-export default page
