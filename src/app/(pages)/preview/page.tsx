43365aa2a7e8b37d4466de7fe2e94ce4d83529d4
hellen-Tchovelidze <elenechovelidze@gmail.com>
2025-05-19 13:41:44 +0400

    g

--- src/app/(pages)/preview/page.tsx
+++ src/app/(pages)/preview/page.tsx
@@ -1,10 +1,13 @@
+import PhoneSimulatorPhoneSimulatorSVG from "@/app/Common/Images/PhoneSimulatorPhoneSimulatorSVG";
 import React from "react";
 
 const page = () => {
   return (
     <>
       <div className="w-full h-[357px] bg-blue-500 absolute top-0 left-0 -z-10 rounded-b-[32px] max-[700px]:hidden"></div>
-        <div></div>
+        <div className=" flex justify-center items-center">
+          <PhoneSimulatorPhoneSimulatorSVG />
+        </div>
     </>
   );
 };
